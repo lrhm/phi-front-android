ce1a02158e323225920307b83e1cb2bee957049a
Ali Rahimi <ilaimihar@gmail.com>
2021-05-05 11:39:10 +0430

    Revert "Apollo cache normalization"
    This reverts commit 86939d33a7e9ca1167aa6916a9d9e646404b1101.

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -14,15 +14,8 @@
 
 # Uncomment this to preserve the line number information for
 # debugging stack traces.
--keepattributes SourceFile,LineNumberTable
+#-keepattributes SourceFile,LineNumberTable
 
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
-#-keepclasseswithmembernames class xyz.lrhm.GetUserQuery.User.*
-
-#-keep class  xyz.lrhm.GetUserQuery$* { *; }
-
-#-keep class com.xxx.A$* {
-#    *;
-#}
--- app/src/main/java/xyz/lrhm/phiapp/hilt/ApolloModule.kt
+++ app/src/main/java/xyz/lrhm/phiapp/hilt/ApolloModule.kt
@@ -1,20 +1,10 @@
+package xyz.lrhm.phiapp.hilt
 
-import android.content.Context
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Operation
-import com.apollographql.apollo.api.ResponseField
-import com.apollographql.apollo.cache.normalized.CacheKey
-import com.apollographql.apollo.cache.normalized.CacheKeyResolver
-import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory
-import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy
-import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCache
-import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory
-import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory
-
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -22,51 +12,11 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class ApolloModule {
 
-
-
     @Provides
     @Singleton
-    fun providesCacheKeyResolver():CacheKeyResolver{
-
-        return object : CacheKeyResolver() {
-            override fun fromFieldRecordSet(field: ResponseField, recordSet: Map<String, Any>): CacheKey {
-                // Retrieve the id from the object itself
-                if(recordSet.containsKey("id"))
-                    return CacheKey.from(recordSet["id"] as String)
-                return CacheKey.NO_KEY
-            }
-
-            override fun fromFieldArguments(field: ResponseField, variables: Operation.Variables): CacheKey {
-                // Retrieve the id from the field arguments.
-                // In the example, this allows to know that `author(id: "author1")` will retrieve `author1`
-                // That sounds straightforward but without this, the cache would have no way of finding the id before executing the request on the
-                // network which is what we want to avoid
-                if(field.resolveArgument("id", variables) != null)
-                    return CacheKey.from(field.resolveArgument("id", variables) as String)
-                return CacheKey.NO_KEY
-            }
-        }
-    }
-
-    @Provides
-    @Singleton
-    fun providesApolloSQL(@ApplicationContext context: Context) = SqlNormalizedCacheFactory(context, "apollo.db")
-
-    @Provides
-    @Singleton
-    fun providesNormilizedCache(sqlNormalizedCacheFactory: SqlNormalizedCacheFactory): NormalizedCacheFactory<LruNormalizedCache> {
-
-        return LruNormalizedCacheFactory(
-            EvictionPolicy.builder().maxSizeBytes(10 * 1024 * 1024).build()
-        ).chain(sqlNormalizedCacheFactory)
-    }
-    @Provides
-    @Singleton
-    fun providesApolloClient(normalizedCache: NormalizedCacheFactory<LruNormalizedCache>, cacheKeyResolver: CacheKeyResolver) = ApolloClient.builder()
+    fun providesApolloClient() = ApolloClient.builder()
         .serverUrl("http://192.168.2.5:4000")
-        .normalizedCache(normalizedCache, cacheKeyResolver)
         .build()
-
 
 
 }
